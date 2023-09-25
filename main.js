c14e68d6182e8f1d12517321bb888d5161324a17
Petrik Diak <diak@petrik.hu>
2023-09-25 17:58:46 +0200

    js,css,

--- main.js
+++ main.js
@@ -1,1 +1,35 @@
 import './style.css'
+
+document.addEventListener("DOMContentLoaded", kezd);
+
+function kezd() {
+const a = document.getElementById("szamit");
+a.addEventListener("click",teruleteskeruletszamit); 
+}
+
+function teruleteskeruletszamit()
+{
+    const a = parseFloat(document.getElementById("aOldal").value);
+    const b = parseFloat(document.getElementById("bOldal").value);
+    const c = parseFloat(document.getElementById("cOldal").value);
+    const eredmeny = document.getElementById("eredmeny");
+    if(a+b <=c || a+c <= b || b+c <= a){
+        eredmeny.textContent = "hibás adatok% A háromszög nem szerkezthető!";
+    }
+    else {
+        const haromszog = {
+            a: a,
+            b: b,
+            c: c,
+            kerulet: function ()
+            {
+                return this.a + this.b + this.c;
+            },
+            terulet: function()
+            {
+            const s = this.kerulet() / 2;
+            return Math.sqrt(s*(s-this.a) * (s - this.b)* (s - this.c));
+            }
+        };
+    }
+}
